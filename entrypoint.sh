901ca1c8c214152425e0ff97f69fb6568802a0b5
Mika Vormisto <mvormisto@mailbox.org>
2023-09-06 09:24:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh -l
 
 set -e
+args=$1
+directory=$2
+
+if [ -n $directory ]; then
+  echo "Input directory provided:"
+  echo "$directory"
+  cd "$directory"
+fi
+
+# Print working directory for easy debugging
+pwd
 
 if [ -z "$APPCENTER_ACCESS_TOKEN" ]; then
     echo "APPCENTER_ACCESS_TOKEN is required to run commands with the appcenter cli"
     exit 126
 fi
 
-
-sh -c "appcenter codepush $*"
+sh -c "appcenter codepush $args"
